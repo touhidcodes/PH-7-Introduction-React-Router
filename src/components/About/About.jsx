4f19c87bd971f03cf0574ea39e7770a284020831
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-15 21:20:54 +0600

    intro-react-router

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+           <h1>This is pure about page, Don't be foolish, seriously?</h1> 
+        </div>
+    );
+};
+
+export default About;<h1>This is pure about page, Don't be foolish, seriously?</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App></App>,
+	},
+	{
+		path: "/about",
+		element: <About></About>,
+	},
+	{
+		path: "/contact",
+		element: <Contact></Contact>,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App></App>
+	</React.StrictMode>
+);
